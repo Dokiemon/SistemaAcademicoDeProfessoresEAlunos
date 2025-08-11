e18fe7d27deebc4c9e9f8c651510cef324b8a907
Isaac Estolano <131707642+IsaacEstolano@users.noreply.github.com>
2025-08-11 17:59:16 +0000

    continuar o get value no cadastro.js

---
+++ ROOT/cadastro.js
@@ -0,0 +1,15 @@
+let userName= document.querySelector(".username");
+let password= document.querySelector("password");
+
+const exeCadastro= async()=>{
+    try{
+        const getValue= ()=>{
+            
+        }
+        const valueUser= userName.value;
+        const valeuPassword= password.value;
+    }
+    catch(){
+
+    }
+}
